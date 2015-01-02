1cb806a6145559f5dca14024e48313ee73dc80e0
tizz98 <dev.tizz98@gmail.com>
2015-01-02 17:03:59 -0500

    registered link & vote in admin

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Link, Vote
 
-# Register your models here.
+class LinkAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Link, LinkAdmin)
+
+class VoteAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Vote, VoteAdmin)
