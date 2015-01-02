cbd6d582b0985fa64541a4e57279affe548359f0
tizz98 <dev.tizz98@gmail.com>
2015-01-02 18:28:55 -0500

    added user profile detail view

--- links/views.py
+++ links/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Link
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import get_user_model
+from .models import Link, UserProfile
 
 class LinkListView(ListView):
 	model       = Link
 	queryset    = Link.with_votes.all()
 	paginate_by = 3
+
+class UserProfileDetailView(DetailView):
+	model         = get_user_model()
+	slug_field    = 'username'
+	template_name = 'user_detail.html'
+
+	def get_object(self, queryset=None):
+		user = super(UserProfileDetailView, self).get_object(queryset)
+		UserProfile.objects.get_or_create(user=user)
+		return user
