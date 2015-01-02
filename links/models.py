c11506ed4496ff76c4adf88f90b169791bcec282
tizz98 <dev.tizz98@gmail.com>
2015-01-02 18:19:29 -0500

    added profile creation signals

--- links/models.py
+++ links/models.py
@@ -33,3 +33,10 @@ class UserProfile(mdoels.Model):
 
 	def __unicode__(self):
 		return "%s's profile" % self.user
+
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		profile, created = UserProfile.objects.get_or_create(user=instance)
+
+from django.db.models.signals import post_save
+post_save.connect(create_profile, sender=User)
