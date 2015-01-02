e5757646547f9d3e88d1b3d46c25ccf4303a357d
tizz98 <dev.tizz98@gmail.com>
2015-01-02 17:56:11 -0500

    added login/logout urls

--- steelrumors/urls.py
+++ steelrumors/urls.py
@@ -4,5 +4,11 @@ from links.views import LinkListView
 
 urlpatterns = patterns('',
     url(r'^$', LinkListView.as_view(), name='home'),
+
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+    'template_name': 'login.html'}, name="login"),
+	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
+	    name="logout"),
+
     url(r'^admin/', include(admin.site.urls)),
 )
