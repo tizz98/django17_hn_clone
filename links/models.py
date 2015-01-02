98ee85f4197b8947a98a112e07fb6649474210ad
tizz98 <dev.tizz98@gmail.com>
2015-01-02 16:54:21 -0500

    added Link model

--- links/models.py
+++ links/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Link(models.Model):
+	title        = models.CharField("Headline", max_length=100)
+	submitter    = models.ForeignKey(User)
+	submitted_on = models.DateTimeField(auto_now_add=True)
+	rank_score   = models.FloatField(default=0.0)
+	url          = models.URLField("URL", max_length=250, blank=True)
+	description  = models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.title
