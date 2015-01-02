b616ac02c86f71a7d016302480d73b8f0dc6f83b
tizz98 <dev.tizz98@gmail.com>
2015-01-02 18:31:17 -0500

    added user profile url

--- steelrumors/urls.py
+++ steelrumors/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from links.views import LinkListView
+from links.views import LinkListView, UserProfileDetailView
 
 urlpatterns = patterns('',
     url(r'^$', LinkListView.as_view(), name='home'),
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name="login"),
 	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
 	url(r'^accounts/', include('registration.backends.simple.urls')),
+	url(r'^users/(?P<slug>\w+)/$', UserProfileDetailView.as_view(), name='profile'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
