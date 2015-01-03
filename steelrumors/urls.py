30e7d216a6c16d2d06c7697fae5497ce82aa797b
tizz98 <dev.tizz98@gmail.com>
2015-01-02 19:01:09 -0500

    added link creation url

--- steelrumors/urls.py
+++ steelrumors/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from links.views import LinkListView, UserProfileDetailView, UserProfileEditView
+from links.views import LinkListView, UserProfileDetailView, UserProfileEditView, LinkCreateView
 from django.contrib.auth.decorators import login_required as auth
 
 urlpatterns = patterns('',
@@ -8,9 +8,12 @@ urlpatterns = patterns('',
 
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name="login"),
 	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
+
 	url(r'^accounts/', include('registration.backends.simple.urls')),
 	url(r'^users/(?P<slug>\w+)/$', UserProfileDetailView.as_view(), name='profile'),
 	url(r'^edit_profile/$', auth(UserProfileEditView.as_view()), name="edit_profile"),
+
+	url(r'^link/create/$', auth(LinkCreateView.as_view()), name="link_create")
 
     url(r'^admin/', include(admin.site.urls)),
 )
